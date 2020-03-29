3b64bbbbb3aa8b2a7bd26926cea205c4a66e636f
drosatto <danielrosatto@gmail.com>
2020-03-29 18:43:41 -0300

    Se corrige asset y se relativiza versionado de docker

--- gidappf/app/controllers/about_controller.rb
+++ gidappf/app/controllers/about_controller.rb
@@ -15,18 +15,7 @@ class AboutController < ApplicationController
 skip_before_action :authenticate_user!, :only => [:team]
 
   def team
-    @logo_ticapps=ProfileValue.new
-    @logo_ticapps.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/logo-TICAPPS-2019-01.jpg")), filename: 'noimg')
-    @logo_iia_unaj=ProfileValue.new
-    @logo_iia_unaj.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/logo-iia-unaj.png")), filename: 'noimg')
-    @user0=ProfileValue.new
-    @user0.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/user0.png")), filename: 'noimg')
-    @user1=ProfileValue.new
-    @user1.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/user1.png")), filename: 'noimg')
-    @user2=ProfileValue.new
-    @user2.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/user3.png")), filename: 'noimg')
-    @user3=ProfileValue.new
-    @user3.active_stored.attach(io: File.open(Rails.root.join("storage/seeds/images/layout/user2.jpg")), filename: 'noimg')
+    # action definition
   end
 
 end
